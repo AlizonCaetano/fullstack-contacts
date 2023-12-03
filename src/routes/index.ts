4003e111dc097c791add3a7a9e6e87829af62d2b
Alison C <alizoncaetano@gmail.com>
2023-12-03 14:38:26 -0300

    add cors and express types, create main routes file

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { Request, Response } from 'express'
+
+const routes = Router()
+
+routes.get('/', (req: Request, res: Response)=>{
+    return res.status(200).json({'message':'Endpoint online'})
+})
+
+export { routes }
