745f660533a49b13be9ec5ffb7931ea34d56f09c
Alison C <alizoncaetano@gmail.com>
2023-12-03 16:11:02 -0300

    fix: adding new route for users, link userController in the new file user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express'
-
-import { UserController } from '../controllers/UserController'
+import { userRoutes } from './users.routes'
 
 const routes = Router()
 
-const userController = new UserController()
-
-routes.post('/users', userController.create)
-
+routes.use('/users', userRoutes)
 
 export { routes }
